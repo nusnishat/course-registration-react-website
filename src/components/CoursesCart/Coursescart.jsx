01d495725991d0001f566c9d16969e430246cbe1
unknown <nusnishat1999@gmail.com>
2024-07-08 10:10:45 +0600

    updated

--- src/components/CoursesCart/Coursescart.jsx
+++ src/components/CoursesCart/Coursescart.jsx
@@ -4,14 +4,14 @@ import Coursecart from '../CourseCart/Coursecart';
 const Coursescart = ({selectCourses, creditHour, creditPrice, creditRemaining}) => {
     return (
         <div className='md: ms-4 w-1/3 mx-auto bg-white rounded-md p-4 space-y-4'>
-            <h1 className='text-blue-700 font-semibold mb-4'>Credit hour remaining : {creditRemaining}</h1>
+            <h1 className='text-blue-700 font-semibold mb-4'>Credit hour remaining : {creditRemaining} hr</h1>
             <hr />
             <h1 className='font-semibold'>Course Name: </h1>
             {
                 selectCourses.map((selectCourse, indx) => <Coursecart key={indx} index={indx} selectCourse={selectCourse}></Coursecart>)
             }
             <hr />
-            <h1>Total credit hour: {creditHour}</h1>
+            <h1>Total credit hour: {creditHour} hr</h1>
             <hr />
             <h1>Total price: {creditPrice} </h1>
         </div>
